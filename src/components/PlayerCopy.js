06620065323b79aa366d2bff8b1a4cf0678934ba
maklad42 <maklad42+git@gmail.com>
2022-04-23 22:25:39 +0900

    Adds draggability to the PlayerCopy components. Next need to work out how to move them around. 220423

--- src/components/PlayerCopy.js
+++ src/components/PlayerCopy.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useDrag } from 'react-dnd';
 
 function PlayerCopy({ clr, id }) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'playercopy',
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const idx = Math.floor(Math.random() * 10000 + 1).toString(16);
   return (
     // eslint-disable-next-line prettier/prettier
-    <div className={`player ${clr}`} draggable key={id} />
+    <div
+      id={idx}
+      className={`player ${clr}`}
+      draggable
+      key={id}
+      ref={drag}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
+    />
   );
 }
 
