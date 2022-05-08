1d172463d801bf040f150c60f5a29c2cbd2f4a18
maklad42 <maklad42+git@gmail.com>
2022-05-08 21:45:44 +0900

    Adds react-draggable package to make the dropped players draggable on the pitch. 220508

--- src/components/PlayerCopy.js
+++ src/components/PlayerCopy.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { useDrag } from 'react-dnd';
+import Draggable from 'react-draggable';
 
 function PlayerCopy({ clr, typ, id }) {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: 'playercopy',
-    item: { clr, typ },
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
+  // const [{ isDragging }, drag] = useDrag(() => ({
+  //   type: 'playercopy',
+  //   item: { clr, typ },
+  //   collect: (monitor) => ({
+  //     isDragging: !!monitor.isDragging(),
+  //   }),
+  // }));
 
   const idx = Math.floor(Math.random() * 10000 + 1).toString(16);
   return (
-    // eslint-disable-next-line prettier/prettier
-    <div
-      id={idx}
-      className={`${typ} copy ${clr}`}
-      draggable
-      key={id}
-      ref={drag}
-      style={{ opacity: isDragging ? 0.5 : 1 }}
-    />
+    <Draggable>
+      {/* eslint-disable-next-line prettier/prettier */}
+      <div
+        id={idx}
+        className={`${typ} copy ${clr}`}
+        draggable
+        key={id}
+        // ref={drag}
+        // style={{ opacity: isDragging ? 0.5 : 1 }}
+      />
+    </Draggable>
   );
 }
 
